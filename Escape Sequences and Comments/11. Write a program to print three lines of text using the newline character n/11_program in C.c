2d3df41d61841aa9abc85b02cf13a359b64b0b21
MD Milon Miah <115646187+mdMilon123@users.noreply.github.com>
2026-02-11 01:10:02 +0600

    Add files via upload

---
+++ Escape Sequences and Comments/11. Write a program to print three lines of text using the newline character n/11_program in C.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include<conio.h>
+int main() {
+    printf("This is line 1.\nThis is line 2.\nThis is line 3.\n");
+    getch();
+}
+
---
+++ Escape Sequences and Comments/12. Write a program to print the sentence/12_Program in C.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include<conio.h>
+int main() {
+    printf("They call him \"John\".\n");
+    getch();
+}
+
